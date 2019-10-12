914d0036e35132ad3e7c3737a04f3e2171b3fb55
Shipra <drshipradyundi@gmail.com>
2019-10-11 17:39:39 -0700

    adding Gusses left counter in the html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,7 +8,6 @@ var Wins = 0;
 var Losses = 0;
 var guessesLeft = 9;
 var guestChoice = [];
-//var i = 0;//
 var computerGuess = ""
 onCharInput();
 
@@ -16,52 +15,34 @@ function onCharInput() {
 
     computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
     console.log("ComputerGuess=", computerGuess);
-    //guessChoices.join(", ") +
     outputWins.innerHTML = "wins :" + Wins;
     outputLosses.innerHTML = "losses :" + Losses;
 
-    //Assigning output variables
     outputUserChoice.innerHTML = "User guesses so far: " + guestChoice;
 
-    //   setTimeout(onCharInput, 0);
-    //  if (i < 10) {
-    //   i++;
-    //   console.log("Looping value of i = ", i);//
 }
 
 document.onkeyup = function(event) {
-        let userGuess = event.key;
-        console.log("userGuess=", userGuess);
-        guestChoice.push(userGuess);
-        outputUserChoice.innerHTML = "User guesses so far: " + guestChoice.join(",");
-        guessesLeft--;
-        outputguessLeft.innerHTML = "guesses left :" + guessesLeft;
+    let userGuess = event.key;
+    console.log("userGuess=", userGuess);
+    guestChoice.push(userGuess);
+    outputUserChoice.innerHTML = "User guesses so far: " + guestChoice.join(",");
+    guessesLeft--;
+    outputguessLeft.innerHTML = "guesses left :" + guessesLeft;
 
-        if (userGuess === computerGuess) {
-            Wins++;
-            // Losses = 0;
-            guessesLeft = 9;
-            guestChoice = [];
-            onCharInput();
-        } else if (guessesLeft === 0) {
-            // Wins = 0;
-            Losses++;
-            guessesLeft = 9;
-            guestChoice = [];
-            onCharInput();
+    if (userGuess === computerGuess) {
+        Wins++;
 
-        }
+        guessesLeft = 9;
+        guestChoice = [];
+        onCharInput();
+    } else if (guessesLeft === 0) {
+
+        Losses++;
+        guessesLeft = 9;
+        guestChoice = [];
+        onCharInput();
 
     }
-    //
-
-
-/*
-var game = {
-    wins: 0,
-    losses: 0,
-    turnsLeft: 9,
-    charsEntered: "",
 
 }
-*/
